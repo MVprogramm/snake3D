3c5a8cfc9fb85debef8ee5e543e576273499ae1b
MVprogramm <michael.valiahmetov@gmail.com>
2025-07-02 07:04:44 +0300

    change Apple.tsx component

--- src/components/Apple.tsx
+++ src/components/Apple.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react'
-import { useGLTF } from '@react-three/drei'
-import { getField } from '../engine/field/fieldPerLevel'
-import { useFrame, Vector3 } from '@react-three/fiber'
-import { getFoodCoord } from '../engine/food/food'
-import { getFoodEaten } from '../engine/events/snakeCatchesFoodEvent'
+import { useGLTF } from '@react-three/drei' // хук для загрузки 3D-модели
+import { getField } from '../engine/field/fieldPerLevel' // получаем размер игрового поля
+import { useFrame } from '@react-three/fiber'
+import { getFoodCoord } from '../engine/food/food' // координаты текущего яблока
 
+/**
+ * Компонент Apple отображает 3D-модель яблока,
+ * получая его позицию на поле из игрового движка.
+ */
 const Apple: React.FC = () => {
-  const { scene } = useGLTF('/apple.glb')
-  const fieldSize = getField()
-  const [foodPosition, setFoodPosition] = React.useState<Vector3>([0, 0, 0.5])
+  const { scene } = useGLTF('/apple.glb') // загружаем сцену с 3D-моделью яблока
+  const fieldSize = getField() // актуальный размер игрового поля
+  const halfField = React.useMemo(() => fieldSize / 2 + 1, [fieldSize])
+  const [foodPosition, setFoodPosition] = React.useState<[number, number, number]>([
+    0, 0, 0.5,
+  ])
 
   useFrame(() => {
     const updatedPosition = getFoodCoord()
-    const adjustedX = Math.round(updatedPosition[0] - fieldSize / 2 - 1)
-    const adjustedY = Math.round(updatedPosition[1] - fieldSize / 2 - 1)
-    const adjustedPosition: Vector3 = [adjustedX, adjustedY, 0.5]
-    setFoodPosition(adjustedPosition)
+    const adjustedX = Math.round(updatedPosition[0] - halfField)
+    const adjustedY = Math.round(updatedPosition[1] - halfField)
+    const adjustedPosition: [number, number, number] = [adjustedX, adjustedY, 0.5]
+    setFoodPosition((prev) =>
+      prev[0] !== adjustedPosition[0] || prev[1] !== adjustedPosition[1]
+        ? adjustedPosition
+        : prev
+    )
   })
 
   React.useEffect(() => {
@@ -24,7 +34,7 @@ const Apple: React.FC = () => {
         node.castShadow = true
       }
     })
-  }, [scene, getFoodEaten()])
+  }, [scene])
 
   return <primitive object={scene} position={foodPosition} scale={0.3} />
 }
