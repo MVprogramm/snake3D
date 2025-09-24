6e8fde62f31a3a92ba4f70840b1c732ed99214d0
MVprogramm <michael.valiahmetov@gmail.com>
2025-09-24 14:38:47 +0300

    made Field.tsx easier

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,14 +1,12 @@
-import { useMemo } from 'react'
 import { fieldConfig } from '../config/fieldConfig'
 import { getField } from '../engine/field/fieldPerLevel'
 import { Grid } from './Grid'
 
 export const Field = () => {
   const { position, backgroundColor } = fieldConfig
-  const fieldSize = useMemo(() => getField(), [])
-  const fieldDimensions = useMemo((): [number, number] => {
-    return [fieldSize, fieldSize]
-  }, [fieldSize])
+  const fieldSize = getField()
+  const fieldDimensions: [number, number] = [fieldSize, fieldSize]
+
   return (
     <group>
       <Grid />
