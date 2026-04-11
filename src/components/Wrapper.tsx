470320005de814edb7ade022ccb2dc44bcf516fe
MVprogramm <michael.valiahmetov@gmail.com>
2026-04-11 10:25:48 +0300

    fixed swipe functionality

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import GameInfo from './GameInfo'
 import { swipeDirectionEvent } from '../engine/events/swipeDirectionEvent'
 import { setTouch } from '../engine/events/touchEvent'
-import protocolExecutor from '../engine/protocol/protocolExecutor'
+import keyboardEvents from '../engine/events/keyboardEvents'
 // import GameButtons from "../GameButtons/GameButtons";
 import { useMenuStore } from '../store/menuStore'
 import '../styles/wrapper.css'
@@ -16,7 +16,10 @@ export function Wrapper({ children }: { children: ReactNode }) {
   const endTouch = (e: React.TouchEvent<HTMLDivElement>) => {
     const touches = e.changedTouches
     setTouch('end', touches[0].clientX, touches[0].clientY)
-    if (!isVisible) protocolExecutor(swipeDirectionEvent())
+    if (!isVisible) {
+      const swipeCode = swipeDirectionEvent()
+      if (swipeCode) keyboardEvents({ code: swipeCode } as KeyboardEvent)
+    }
   }
   return (
     <div className='wrapper' onTouchStart={startTouch} onTouchEnd={endTouch}>
